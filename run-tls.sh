f59b54c0c8d3b7842a15bbfdee6d844edafebf02
Waaoooww <alfinpoetra48@gmail.com>
2026-09-21 21:34:34 +0700

    Keluarkan script run-* dari tracking

--- run-tls.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exec socat OPENSSL-LISTEN:8445,cert=/home/alfin/latekaje-v3/ssl-v2/v2.pem,verify=0,fork TCP:127.0.0.1:8002
--- run-v2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/alfin/latekaje-v3
-exec php artisan serve --host=0.0.0.0 --port=8002
